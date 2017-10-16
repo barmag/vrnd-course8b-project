ff013ad60ad0834a41a5236fc50a8b7f81dfbb62
Yasser Makram <yasser@barm.ag>
2017-10-16 09:00:02 +0200

    Cycle through pooled balls

--- Assets/PerformanceBounceback/Scripts/BallSpawner.cs
+++ Assets/PerformanceBounceback/Scripts/BallSpawner.cs
@@ -39,14 +39,14 @@ public GameObject GetPooledBall()
         ballPoolNum = 0;
     }
     //if we’ve run out of objects in the pool too quickly, create a new one
-    if (pooledBalls[ballPoolNum].activeInHierarchy)
-    {
-        //create a new bullet and add it to the bulletList
-        GameObject obj = Instantiate(pooledBall);
-        pooledBalls.Add(obj);
-        ballsAmount++;
-        ballPoolNum = ballsAmount - 1;
-    }
+    //if (pooledBalls[ballPoolNum].activeInHierarchy)
+    //{
+    //    //create a new bullet and add it to the bulletList
+    //    GameObject obj = Instantiate(pooledBall);
+    //    pooledBalls.Add(obj);
+    //    ballsAmount++;
+    //    ballPoolNum = ballsAmount - 1;
+    //}
         //Debug.Log(ballPoolNum);
         return pooledBalls[ballPoolNum];
 }
